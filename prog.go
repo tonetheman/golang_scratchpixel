0160414c04be4dd45fdb492d68248a32a41a7156
Tony Colston <tonetheman@gmail.com>
2018-03-07 14:22:06 -0600

    more stuff

--- prog.go
+++ prog.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -37,5 +38,32 @@ func (v Vec3) length2() float64 {
 	return v.x*v.x + v.y*v.y + v.z*v.z
 }
 
+type Sphere struct {
+	center                      Vec3
+	radius, radius2             float64
+	surfaceColor, emissionColor Vec3
+	transparency, reflection    float64
+}
+
+func newSphere(c Vec3, r float64, sc Vec3, ref, tr float64) Sphere {
+	return Sphere{center: c, radius: r, radius2: r * r,
+		surfaceColor: sc, reflection: ref, transparency: tr}
+}
+
+func render(spheres []Sphere) {
+	const width = 640
+	const height = 480
+	//var image []Vec3 = make([]Vec3, width*height)
+	//invWidth := 1 / width
+	//invHeight := 1 / height
+}
+
 func main() {
+	var spheres []Sphere
+	spheres = append(spheres, newSphere(Vec3{0, -10004, -20}, 10000,
+		Vec3{0.20, 0.20, 0.20}, 0, 0.0))
+	spheres = append(spheres, newSphere(Vec3{0, 0, -20}, 4,
+		Vec3{1.0, 0.32, 0.36}, 1, 0.5))
+	fmt.Println(spheres)
+
 }
